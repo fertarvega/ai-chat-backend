38a3b554909ad6727095ec7b30c8633326cdf877
Fernando Tarango <fertarvega@gmail.com>
2025-02-07 16:33:04 -0600

    Add redis config to store messages and have continuity and varios chats at the same time

--- server.js
+++ server.js
@@ -1,36 +1,40 @@
-const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import { Redis } from "@upstash/redis";
+import { config } from "dotenv";
 
 const app = express();
 const port = 3000;
+config();
 
 app.use(cors());
 app.use(express.json({ limit: "500mb" }));
 
-let conversationHistory = [];
+const redis = new Redis({
+  url: process.env.URL_REDIS,
+  token: process.env.TOKEN_REDIS,
+});
 
 app.post("/chat", async (req, res) => {
   try {
-    const { image, prompt } = req.body;
-
+    const { image, prompt, uuid } = req.body;
     if (!prompt) {
       return res.status(400).json({ error: "Se requiere un prompt" });
     }
+    const conversationHistory = (await redis.get(uuid)) || [];
 
     conversationHistory.push({ role: "user", content: prompt });
-
     let requestData = {
       model: "llama3.2",
       prompt: conversationHistory
         .map((entry) => `${entry.role}: ${entry.content}`)
         .join("\n"),
+      // stream: false,
     };
-
     // if (image) {
     //   requestData.images = [image.split(",")[1]];
     // }
-
     const response = await axios.post(
       "http://localhost:11434/api/generate",
       requestData,
@@ -38,11 +42,11 @@ app.post("/chat", async (req, res) => {
     );
 
     let fullResponse = "";
-
     response.data.on("data", (chunk) => {
       const data = JSON.parse(chunk.toString());
       if (data.done) {
         conversationHistory.push({ role: "assistant", content: fullResponse });
+        redis.set(uuid, conversationHistory);
         res.end();
       } else {
         fullResponse += data.response;
@@ -60,9 +64,14 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-app.get("/chat/reset", async (req, res) => {
-  res.json({ conversationHistory });
-  conversationHistory = [];
+app.get("/chat/delete", async (req, res) => {
+  try {
+    const { uuid } = req.query;
+    redis.del(uuid);
+    res.status(200).json({ message: "Deleted on db" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
